416a2fc51ce0c7253f1a501908561b2d2e4ac899
Carlos Garzon <showlove@knights.ucf.edu>
2020-03-27 16:59:04 -0400

    added gtest to syntax

---
+++ syntax/utils/tests/CMakeFiles/gtestProject.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/gtestProject.dir/gtestsMain.cpp.o"
+  "CMakeFiles/gtestProject.dir/__/cStringUtils.cpp.o"
+  "CMakeFiles/gtestProject.dir/cStringUtilsTest.cpp.o"
+  "gtestProject.pdb"
+  "gtestProject"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/gtestProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
